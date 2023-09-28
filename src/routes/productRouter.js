f4e2e586574154d67f8e2de7d5266930abf0e1ec
L337Crew <82683115+L337Crew@users.noreply.github.com>
2023-09-28 16:31:33 -0300

    practicaIntegradora2c

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,49 +1,100 @@
 import { Router } from "express";
 import { productService } from "../dao/index.js";
 
-const validateFields = (req,res,next)=>{
-    const productInfo = req.body;
-    if(!productInfo.title || !productInfo.price){
-        return res.json({status:"error", message:"campos incompletos"})
-    } else {
-        next();
-    }
+const validateFields = (req, res, next) => {
+  const productInfo = req.body;
+  if (!productInfo || !productInfo.title || !productInfo.price) {
+    return res.json({ status: "error", message: "Campos incompletos" });
+  }
+  next();
 };
 
 const router = Router();
 
-router.get("/", async(req,res)=>{
-    try {
-        const limit = req.query.limit;
-        const products = await productService.get();
-        if(limit){
-            //devolver productos de acuerdo al limite
-        } else {
-            res.json({status:"success", data:products});
-        }
-    } catch (error) {
-        res.json({status:"error", message:error.message});
+router.get("/", async (req, res) => {
+  try {
+    const limit = req.query.limit;
+    const products = await productService.get();
+
+    if (limit) {
+      const limitedProducts = products.slice(0, parseInt(limit));
+      res.json({ status: "success", data: limitedProducts });
+    } else {
+      res.json({ status: "success", data: products });
     }
+  } catch (error) {
+    res.json({ status: "error", message: error.message });
+  }
 });
 
-router.get("/:pid",(req,res)=>{});
+router.get("/:pid", async (req, res) => {
+  try {
+    const productId = req.params.pid;
+    const product = await productService.getById(productId);
 
-router.post("/", validateFields, async(req,res)=>{
-    //Agregar el producto
-    try {
-        const productInfo = req.body;
-        const productCreated = await productService.save(productInfo);
-        res.json({status:"success", data:productCreated, message:"producto creado"});
-    } catch (error) {
-        res.json({status:"error", message:error.message});
+    if (!product) {
+      return res.json({ status: "error", message: "Producto no encontrado" });
     }
+
+    res.json({ status: "success", data: product });
+  } catch (error) {
+    res.json({ status: "error", message: error.message });
+  }
 });
 
-router.put("/:pid",validateFields,(req,res)=>{
+router.post("/", validateFields, async (req, res) => {
+  try {
     const productInfo = req.body;
-    //actualiza el producto
+    const productCreated = await productService.save(productInfo);
+    res.status(201).json({
+      status: "success",
+      data: productCreated,
+      message: "Producto creado",
+    });
+  } catch (error) {
+    res.json({ status: "error", message: error.message });
+  }
 });
 
-router.delete("/:pid",(req,res)=>{});
+router.put("/:pid", validateFields, async (req, res) => {
+  try {
+    const productId = req.params.pid;
+    const updatedData = req.body;
 
-export {router as productsRouter}
+    const updatedProduct = await productService.update(productId, updatedData);
+
+    if (!updatedProduct) {
+      return res.json({ status: "error", message: "Producto no encontrado" });
+    }
+
+    res.json({
+      status: "success",
+      data: updatedProduct,
+      message: "Producto actualizado",
+    });
+  } catch (error) {
+    res.json({ status: "error", message: error.message });
+  }
+});
+
+router.delete("/:pid", async (req, res) => {
+  try {
+    const productId = req.params.pid;
+
+    const deletedProduct = await productService.delete(productId);
+
+    if (!deletedProduct) {
+      return res.json({ status: "error", message: "Producto no encontrado" });
+    }
+
+    res.json({
+      status: "success",
+      data: deletedProduct,
+      message: "Producto eliminado",
+    });
+  } catch (error) {
+    res.json({ status: "error", message: error.message });
+  }
+});
+
+export { router as productsRouter };
