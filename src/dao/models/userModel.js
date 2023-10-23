c730a47056332a9527a153146c161bf5987cfeb8
L337Crew <82683115+L337Crew@users.noreply.github.com>
2023-10-23 16:10:09 -0300

    practicaIntegradora2d

--- src/dao/models/userModel.js
+++ src/dao/models/userModel.js
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose';
+import passportLocalMongoose from "passport-local-mongoose"
 
 const userSchema = new mongoose.Schema({
   username: String,
   email: String,
-  roles: { type: String, default: 'usuario' },
+  roles: [{ type: String, default: 'usuario' }], // Debe ser un Array de Strings
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 export default mongoose.model('User', userSchema);
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -4,6 +4,25 @@ import User from '../dao/models/userModel.js';
 
 const authRouter = express.Router();
 
+// Middleware para verificar el rol del usuario
+function checkRole(role) {
+  return async (req, res, next) => {
+    if (req.isAuthenticated()) {
+      try {
+        const user = await User.findById(req.user._id);
+        if (user.roles === role) {
+          return next();
+        }
+        res.status(403).json({ error: 'Acceso no autorizado' });
+      } catch (error) {
+        res.status(500).json({ error: 'Error al verificar el rol del usuario' });
+      }
+    } else {
+      res.status(401).json({ error: 'Debe iniciar sesión' });
+    }
+  };
+}
+
 // Ruta para mostrar el formulario de inicio de sesión
 authRouter.get('/login', (req, res) => {
   res.render('login'); // Asegúrate de que tengas una vista 'login.handlebars' en tu carpeta 'views'
@@ -20,7 +39,7 @@ authRouter.post('/register', async (req, res) => {
   }
 });
 
-// Ruta para procesar el inicio de sesión del usuario
+// Ruta para procesar el inicio de sesión del usuario y verificar el rol
 authRouter.post('/login', passport.authenticate('local', { failureRedirect: '/auth/login' }), (req, res) => {
   res.redirect('/api/products'); // Redirige a la página de productos después del inicio de sesión exitoso
 });
